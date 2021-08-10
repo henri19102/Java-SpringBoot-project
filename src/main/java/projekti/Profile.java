c7374b046460b56d94f94d8614e477fad4cdec99
Henri Mattila <henri19102@student.hamk.fi>
2021-08-10 21:33:29 +0300

    Start of the java project

---
+++ src/main/java/projekti/Profile.java
@@ -0,0 +1,15 @@
+package projekti;
+
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profile extends AbstractPersistable<Long> {
+    String name;
+}
